d62ed19d723706317280b5a40f90f4af806f0996
Tiefighter345 <tommygrenet@gmail.com>
2025-12-12 14:56:35 +0100

    ALPHA.3.0

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #IMPORTS
 import json
-from os import listdir, makedirs
+from os import listdir, makedirs, system
 from os.path import isfile, join, isdir
 
 #Load language
-def load_langauge():
+def select_langauge():
     #Display available languages
     languages = [f for f in listdir("Languages") if isfile(join("Languages", f))]
     if len(languages) != 0:
@@ -38,6 +38,7 @@ def load_langauge():
             language_data = json.load(data)
             language_name = language_data["name"]
         
+        system("cls")
         print(f"you selected '{language_name}'")
         return language_data
     
@@ -46,20 +47,6 @@ def load_langauge():
         return None
 
 
-#Create a new language
-def new_language():
-    #name of the language
-    name = input("What is the name of your language?\n>")
-    
-    #saving the name to the language folder
-    data = {
-        "name": name,
-        "words": []
-        }
-    
-    save_language(data)
-    
-
 #Save language data
 def save_language(data):
     name = data["name"]
@@ -67,62 +54,86 @@ def save_language(data):
         json.dump(data, language)
 
 
+#Create a new language
+def new_language():
+    #name of the language
+    name = input("What is the name of your language? (or type 'exit' to cancel)\n>")
+    
+    #saving the name to the language folder
+    if name != "exit":
+        data = {
+            "name": name,
+            "words": []
+            }
+        
+        save_language(data)
+        system("cls")
+        print("Language created\n\n")
+    
+    else: system("cls")
+
+
 #Edit an existing language
 def edit_language():
-    language = load_langauge()
+    language = select_langauge()
     if language == None: return
-
-    #edit type
-    flag = True
-    print("what type of edit do you want to do? (n for add word, d for delete word)")
-    while flag:
-        edit_action = input(">")
-        if edit_action != "n" and edit_action != "d":
-            print("Type a valid input (n for add new ford or d for delete existing word)")
-        else: flag = False
-
-    #add new word
-    if edit_action == "n":
-        print("What is the word in your language?")
-        word = input(">")
-
-        print("What is the word in English")
-        translation = input(">")
-
-        new_data = [word, translation]
-
-        language["words"].append(new_data)
     
-    #delete an existing word
-    else:
-        #load and display pairs
-        words = language["words"]
-        for i in range(len(words)):
-            pair = words[i]
-            print(f"{i+1}: {pair[0]}, translating to: {pair[1]}")
-        print("Which pair do you want to delete? (number)")
-        
-        #exception catching loop
-        flag1 = True
-        flag2 = True
+    looping = True
+    while looping:
+        #edit input
+        flag = True
+        print("'n' to add a new word, 'd' to delete an existing pair, 'exit' to go back to main menu")
+        while flag:
+            edit_action = input(">")
+            if edit_action != "n" and edit_action != "d" and edit_action != "exit":
+                print("Type a valid input (n or d or exit)")
+            else: flag = False
 
-        while flag1:        
-            while flag2:
-                selector = input(">")
-                try: selector = int(selector)
-                except: 
+        #add new word
+        if edit_action == "n":
+            print("What is the word in your language?")
+            word = input(">")
+
+            print("What is the word in English")
+            translation = input(">")
+
+            new_data = [word, translation]
+
+            language["words"].append(new_data)
+        
+        #delete an existing word
+        elif edit_action == "d":
+            #load and display pairs
+            words = language["words"]
+            for i in range(len(words)):
+                pair = words[i]
+                print(f"{i+1}: {pair[0]}, translating to: {pair[1]}")
+            print("Which pair do you want to delete? (number)")
+            
+            #exception catching loop
+            flag1 = True
+            flag2 = True
+            while flag1:        
+                while flag2:
+                    selector = input(">")
+                    try: selector = int(selector)
+                    except: 
+                        print("input a valid number")
+                    else: flag2 = False
+
+                if selector <= 0 or selector > len(words):
                     print("input a valid number")
-                else: flag2 = False
-
-            if selector <= 0 or selector > len(words):
-                print("input a valid number")
-                flag2 = True
-            else:
-                flag1 = False
-                selector -= 1
+                    flag2 = True
+                else:
+                    flag1 = False
+                    selector -= 1
+            
+            #delete chosen pair
+            words.pop(selector)
         
-        #delete chosen pair
-        words.pop(selector)
+        #exit the loop
+        else:
+            looping = False
 
 
     save_language(language)
@@ -135,20 +146,24 @@ def Main():
     if not isdir("Languages"):
         makedirs("Languages")
 
-    flag = True
-    print("Do you want to create a new language or edit an exist one? (n for new or e for edit)")
-    while flag:
-        user_input = input(">")
-        if user_input == "e" or user_input == "n":
-            flag = False
-        else:
-            print("please input a valid answer (n for new or e for edit)")
+    #main loop
+    running = True
+    while running:
+        flag = True
+        print("'n' to add a new language, 'e' to edit an existing one, 'exit' to exit")
+        while flag:
+            user_input = input(">")
+            if user_input == "e" or user_input == "n" or user_input == "exit":
+                flag = False
+            else:
+                print("please input a valid answer (n for new or e for edit, 'exit' to exit)")
 
-    if user_input == "n":
-        new_language()
-    else:
-        edit_language()
-
+        system("cls")
+        if user_input == "n":
+            new_language()
+        elif user_input == "e":
+            edit_language()
+        else: running = False
 
     print("Goodbye")
 
